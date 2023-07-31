8b211856e1764722f6b0ecd54f198ed8e42119d4
Abdullah Habib <abdullahhabib.86@gmail.com>
2023-07-31 16:54:21 +0500

    added requirments.txt

--- app.py
+++ app.py
@@ -65,9 +65,3 @@ def chat_me(request: ChatRequest):
 @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
-
-# if __name__ == "__main__":
-#     import uvicorn
-
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
